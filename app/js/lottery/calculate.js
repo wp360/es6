b6ed1275956f2607edc12d262bd9cbe83fdcb3c9
wp360 <2897406527@qq.com>
2017-08-27 23:59:41 +0800

    add file

--- app/js/lottery/calculate.js
+++ app/js/lottery/calculate.js
@@ -16,6 +16,52 @@ class Calculate{
         return count;
     }
     /**
+     * [computeBonus 计算奖金范围]
+     * @param  {number} active      [当前选中的号码]
+     * @param  {string} play_name   [当前的玩法标识]
+     * @return {array}              [奖金范围 min, max]
+     */
+    computeBonus(active,play_name){
+        const play = play_name.split('');
+        const self = this;
+        let arr = new Array(play[1]*1).fill(0);
+        let min,max;
+        if(play[0]==='r'){
+            let min_active = 5-(11-active);
+            if(min_active>0){
+                if(min_active-play[1]>=0){
+                    arr = new Array(min_active).fill(0);
+                    min = Calculate.combine(arr,play[1]).length;
+                }else{
+                    if(play[1]-5>0&&active-play[1]>=0){
+                        arr = new Array(active-5).fill(0);
+                        min = Calculate.combine(arr,play[1]-5).length;
+                    }else{
+                        min = active-play[1]>-1?1:0;
+                    }
+                }
+            }else{
+                min = active-play[1]>-1?1:0;
+            }
+            let max_active = Math.min(active,5);
+            if(play[1]-5>0) {
+                if (active - play[1] >= 0) {
+                    arr = new Array(active - 5).fill(0);
+                    max = Calculate.combine(arr, play[1] - 5).length;
+                } else {
+                    max = 0;
+                }
+            }else if(play[1]-5<0){
+                arr = new Array(Math.min(active,5)).fill(0);
+                max = Calculate.combine(arr,play[1]).length;
+            }else{
+                max = 1;
+            }
+        }
+        return [min,max].map(item => item*self.play_list.get(play_name).bonus);
+    }
+
+    /**
      * [combine 组合运算]
      * @param  {array}  arr      [参与组合运算的数组]
      * @param  {number} size     [组合运算的基数]
@@ -46,3 +92,5 @@ class Calculate{
         })(arr,size,result);
     }
 }
+
+export default Calculate;
